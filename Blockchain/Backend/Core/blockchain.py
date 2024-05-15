8909ad56bf9cb7df5a95f5736ffea70eac4d44af
Ankit_Regmi <regmiank2002@gmail.com>
2024-05-15 09:02:59 +0545

    addresses and transaction

--- Blockchain/Backend/Core/blockchain.py
+++ Blockchain/Backend/Core/blockchain.py
@@ -4,16 +4,23 @@ sys.path.append('/Users/Ankit Regmi/OneDrive/Desktop/Bitcoin')
 from Blockchain.Backend.Core.Block import Block 
 from Blockchain.Backend.Core.blockheader import BlockHeader 
 from Blockchain.Backend.util.util import hash256
+from Blockchain.Backend.Core.database.database import BlockchainDB
 import time 
-import json
 
 ZERO_HASH = '0' * 64
 VERSION = 1
 
 class Blockchain:
     def __init__(self):
-        self.chain = []
         self.GenesisBlock() 
+
+    def write_on_disk(self, block):
+        blockchainDB = BlockchainDB()
+        blockchainDB.write(block)
+
+    def fetch_last_block(self):
+        blockchainDB = BlockchainDB()
+        return blockchainDB.lastBlock()
 
     def GenesisBlock(self):
         BlockHeight = 0 
@@ -25,16 +32,15 @@ class Blockchain:
         Transaction =f"Ankit sent {BlockHeight} Bitcoins which he doesnot hold to Satoshi"
         merkleRoot = hash256(Transaction.encode()).hex()
         bits = 'ffff001f'
-        blockheader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp,bits)
+        blockheader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits)
         blockheader.mine()
-        self.chain.append(Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__)
-        print(json.dumps(self.chain, indent = 4))
+        self.write_on_disk([Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__])
 
     def main(self): 
         while True: 
-            lastBlock = self.chain[::-1]
-            BlockHeight = lastBlock[0]["Height"]+1
-            prevBlockHash = lastBlock[0]['BlockHeader']['blockHash']
+            lastBlock = self.fetch_last_block()
+            BlockHeight = lastBlock["Height"]+1
+            prevBlockHash = lastBlock['BlockHeader']['blockHash']
             self.addBlock(BlockHeight, prevBlockHash)
 
 if __name__ == "__main__":
